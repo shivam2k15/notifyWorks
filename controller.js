2b56e2c4dfba5147482db49785c5a675b4795e06
shivam2k15 <shivamkumar2k16k@gmail.com>
2025-04-20 19:29:08 +0530

    connected the emails and jobs

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
-// const { getJob } = require("./emailWorker");
+const { getJob } = require("./emailWorker");
+const { pool } = require("./db");
 
 const createPost = async (req, res, next) => {
   try {
@@ -12,19 +13,31 @@ const createPost = async (req, res, next) => {
       });
     }
 
+    const query = {
+      text: "insert into posts (user_id,title,description) values ($1, $2, $3)",
+      values: [
+        userId, // userId1,
+        title, // "why to use postgres",
+        description, // "we should use sql databse for complex relation based table data",
+      ],
+    };
+    const postCreated = await pool.query(query);
+    console.log(postCreated, "post");
+
     const followerEmails = await getFollowerEmails(userId); // Await the promise
+    console.log(followerEmails, "followerEmails");
     if (!followerEmails || followerEmails.length === 0) {
       return res
         .status(200)
         .json({ message: "Post created, but no followers to notify." }); //  No followers is not an error.
     }
 
-    // 2. Add a job to the queue for sending emails
-    // let job = await getJob(followerEmails, title, description);
+    // Add a job to the queue for sending emails
+    let job = await getJob(followerEmails, title, description);
 
-    // console.log(
-    //   `Email job added to queue for post: ${title}, Job ID: ${job.id}`
-    // );
+    console.log(
+      `Email job added to queue for post: ${title}, Job ID: ${job.id}`
+    );
 
     res.status(201).json({
       message: "Post created and notifications queued.",
@@ -40,12 +53,26 @@ const createPost = async (req, res, next) => {
 
 const getFollowerEmails = async (userId) => {
   console.log(`Fetching follower emails for user: ${userId}`);
+  const query = {
+    text: "SELECT email FROM users join followers f on f.follower_id=users.id  WHERE f.user_id = $1",
+    values: [
+      userId, // userId1,
+    ],
+  };
+  const allEmails = await pool.query(query);
+  return allEmails.rows.map(item=>item.email);
 };
 
 const getPosts = async (req, res, next) => {
-  res.json({
-    message: "Hello good to see you happy",
-  });
+  try {
+    const response = await pool.query("select * from posts");
+    res.json(response.rows);
+  } catch (error) {
+    return next({
+      status: 500,
+      message: error.message || "Something went Wrong",
+    });
+  }
 };
 
 const updateNotification = async (req, res, next) => {
--- db.js
+++ db.js
@@ -21,4 +21,4 @@ const connectToDB = () => {
     });
 };
 
-module.exports = { connectToDB };
+module.exports = { connectToDB, pool };
