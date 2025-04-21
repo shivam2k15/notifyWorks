c6148e1c67c6ccef854b08e94d735f1e5f9eb197
shivam2k15 <shivamkumar2k16k@gmail.com>
2025-04-21 13:07:07 +0530

    host change

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(errorHandler);
 
 //  Start the Server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server listening on port ${port}`);
 });
